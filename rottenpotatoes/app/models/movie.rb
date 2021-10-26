e9df735643fd61aa24103fe2c5103c3c6d6c1be0
aidai beishekeeva <ab5248@columbia.edu>
2021-10-26 17:49:36 +0000

    m

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ActiveRecord::Base
+
+  def self.find_by_director id
+    director_name = Movie.find(id).director
+    Movie.where("id != ?", id).where("director= ?",director_name)  
+    
+  end 
 end
